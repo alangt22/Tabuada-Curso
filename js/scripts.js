441b777f5963c5f97ff8a8b908e760658e28590e
alan nunes <alansilva2896@gmail.com>
2024-07-19 15:06:39 -0300

    Feat: incluindo JS para funçoes e eventos

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+//seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+const multiplicationTable = document.querySelector("#multiplication-operations");
+// funções
+const createTable = (number, multiplicatorNumber) => {
+
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i
+       const template = `
+       <div class="row">
+       <div class="operations">${number} x ${i} =</div> 
+       <div class="result">${result}</div>
+       </div>
+       `
+       const parser = new DOMParser()
+       const htmlTenplate = parser.parseFromString(template, "text/html" )
+       const row = htmlTenplate.querySelector(".row")
+       multiplicationTable.appendChild(row)
+    }
+
+    multiplicationTitle.innerText = number;
+
+};
+
+//eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+
+});
